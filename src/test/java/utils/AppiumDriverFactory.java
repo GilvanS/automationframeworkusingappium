2b4fdd175505b94120255c017936e36bbe668317
GilvanS <74104232+GilvanS@users.noreply.github.com>
2024-08-13 20:49:38 -0300

    up
    up

--- src/test/java/utils/AppiumDriverFactory.java
+++ src/test/java/utils/AppiumDriverFactory.java
@@ -11,30 +11,30 @@ import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
 
 public class AppiumDriverFactory {
-	
+
 	private static AppiumDriverFactory instanceOfAppiumDriverFactory = null;
 	private AppiumDriver<AndroidElement> driver;
-	
-	
+
+
 	private static String appPackage = ConfigFileReader.getConfigPropertyVal("appPackage");
 	private static String appActivity = ConfigFileReader.getConfigPropertyVal("appActivity");
 	private static String deviceName = ConfigFileReader.getConfigPropertyVal("deviceName");
-	
-	
+
+
 	// Declaring constructor as private to restrict object creation outside of class
 	private AppiumDriverFactory() {
-		DesiredCapabilities capabilities = new DesiredCapabilities();
-		capabilities.setCapability("platformName", "android");
-		capabilities.setCapability("device", deviceName);
-		capabilities.setCapability("appPackage", appPackage);
-		capabilities.setCapability("appActivity", appActivity);
-		capabilities.setCapability("automationName", "UIAutomator2");
-		capabilities.setCapability("noReset", "true");
-		capabilities.setCapability("unicodeKeyboard", true);
+		DesiredCapabilities cap = new DesiredCapabilities();
+		cap.setCapability("platformName", "android");
+		cap.setCapability("device", "0078720227");
+		cap.setCapability("appPackage", "br.com.lojaebac");
+		cap.setCapability("appActivity", "br.com.lojaebac.MainActivity");
+		cap.setCapability("automationName", "UIAutomator2");
+		cap.setCapability("noReset", "true");
+		cap.setCapability("unicodeKeyboard", true);
 		//AppiumDriver driver;
-		
+
 		try {
-			driver = new AndroidDriver<AndroidElement>(new URL("http://127.0.0.1:4723/wd/hub/"), capabilities);
+			driver = new AndroidDriver<AndroidElement>(new URL("http://127.0.0.1:4723/wd/hub/"), cap);
 		} catch (MalformedURLException e) {
 			e.printStackTrace();
 		}
